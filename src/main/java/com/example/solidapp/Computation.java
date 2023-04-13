7f420aafc058b2f17e0420a727f8bbf2c242c7ed
SeraphinaLiang <1010814876@qq.com>
2023-04-13 15:31:46 +0200

    done

--- src/main/java/com/example/solidapp/Computation.java
+++ src/main/java/com/example/solidapp/Computation.java
@@ -20,7 +20,7 @@ public class Computation {
     private String pk0;
     private BigInteger pk0_gmp;
     private ArrayList<String> allIdentifiers = new ArrayList<>(); // all identifiers in the store, plaintext
-    private ArrayList<String> fileContents = new ArrayList<>();
+    private ArrayList<ArrayList<BigInteger>> filesContent = new ArrayList<>();
 
 
     public Computation(ArrayList<String> encryptIdentifier, HashMap<String, String> files, String pk0) {
@@ -43,8 +43,16 @@ public class Computation {
         Iterator iterator = files.keySet().iterator();
 
         while (iterator.hasNext()) {
-            String f = files.get(iterator.next());
-            fileContents.add(f);
+            String f = files.get(iterator.next()); // full text of a file
+            ArrayList<BigInteger> word = new ArrayList<>();
+            String[] fs = f.split("\n"); // every bits
+
+            for (String s : fs) {
+                s = s.replaceAll("[^0-9]", "");
+                BigInteger line = new BigInteger(s);
+                word.add(line);
+            }
+            filesContent.add(word);
         }
     }
 
@@ -83,12 +91,12 @@ public class Computation {
             ArrayList<BigInteger> add_result = new ArrayList<>();
             for (int i = 0; i < plaintextID.size(); i++) {
                 BigInteger i_target = target.get(i);  // ci
-             //   System.out.println("ci: "+i_target);
+                //   System.out.println("ci: "+i_target);
                 BigInteger i_plaintextID = plaintextID.get(i);  // vi
-             //   System.out.println("vi: "+i_plaintextID);
-             //  BigInteger k = new BigInteger(String.valueOf(i_target.add(i_plaintextID).add(one).mod(pk0_gmp).mod(two)));
+                //   System.out.println("vi: "+i_plaintextID);
+                //  BigInteger k = new BigInteger(String.valueOf(i_target.add(i_plaintextID).add(one).mod(pk0_gmp).mod(two)));
                 BigInteger k = new BigInteger(String.valueOf((i_target.add(i_plaintextID).add(one)).mod(pk0_gmp)));// do not mod 2
-             //   System.out.println("k:  "+k);
+                //   System.out.println("k:  "+k);
                 add_result.add(k);
             }
             result.add(add_result);
@@ -97,7 +105,7 @@ public class Computation {
     }
 
     //  calculate localizer
-    public ArrayList<BigInteger> AND_Result(ArrayList<ArrayList<BigInteger>> addResult) {
+    public ArrayList<BigInteger> ANDStep(ArrayList<ArrayList<BigInteger>> addResult) {
         ArrayList<BigInteger> andResult = new ArrayList<>();
 
         for (ArrayList<BigInteger> number : addResult) {
@@ -105,12 +113,64 @@ public class Computation {
             for (int i = 1; i < number.size(); i++) {
                 sum = sum.multiply(number.get(i)).mod(pk0_gmp);
             }
-        //    System.out.println("sum "+ sum);
+            //    System.out.println("sum "+ sum);
             andResult.add(sum);
         }
         return andResult;
     }
 
+    // times localizer to file
+    public ArrayList<ArrayList<BigInteger>> TimesStep(ArrayList<BigInteger> localizers) {
+
+        ArrayList<ArrayList<BigInteger>> result = new ArrayList<>();
+
+        for (int i = 0; i < localizers.size(); i++) {
+            ArrayList<BigInteger> currentResult = new ArrayList<>();
+            BigInteger localizer = localizers.get(i);
+            ArrayList<BigInteger> file = filesContent.get(i);
+
+            for (BigInteger bit : file) {
+                BigInteger k = bit.multiply(localizer).mod(pk0_gmp);
+                currentResult.add(k);
+            }
+            result.add(currentResult);
+        }
+
+        return result;
+    }
+
+    public ArrayList<BigInteger> addupStep(ArrayList<ArrayList<BigInteger>> timesResult) {
+        int index = findmaxIndex(timesResult);
+        ArrayList<BigInteger> result = timesResult.get(index);
+        timesResult.remove(index);
+
+        for (ArrayList<BigInteger> list : timesResult) {
+            for (int i = 0; i < list.size(); i++) {
+                BigInteger cur = list.get(i);
+                BigInteger sum = result.get(i);
+                BigInteger addup = cur.add(sum).mod(pk0_gmp);
+
+                result.remove(i);
+                result.add(i, addup);
+            }
+        }
+      //  System.out.println(result);
+
+        return result;
+    }
+
+    public int findmaxIndex(ArrayList<ArrayList<BigInteger>> timesResult) {
+        int max = -1;
+        int index = -1;
+        for (ArrayList<BigInteger> l : timesResult) {
+            int len = l.size();
+            if (len > max) {
+                max = len;
+                index = timesResult.indexOf(l);
+            }
+        }
+        return index;
+    }
 
     /**
      * Get the computation final result
@@ -120,13 +180,14 @@ public class Computation {
     public ArrayList<String> getResult() {
 
         ArrayList<ArrayList<BigInteger>> addResult = AddingStep();
-        ArrayList<BigInteger> andResult = AND_Result(addResult);  // localizer of every file
-
-
+        ArrayList<BigInteger> andResult = ANDStep(addResult);  // localizer of every file
+        ArrayList<ArrayList<BigInteger>> timesResult = TimesStep(andResult);
+        ArrayList<BigInteger> finalResult = addupStep(timesResult);
 
         ArrayList<String> result = new ArrayList<>();
-        result.add("hello");
-        result.add("world");
+        for (BigInteger i: finalResult) {
+            result.add(i.toString());
+        }
 
         return result;
     }
